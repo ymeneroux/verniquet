3fde519029ccf536ddf138817baf3c91c1a72d30
YMeneroux <yann.meneroux@ign.fr>
2024-01-02 10:13:04 +0100

    Publi fiches

--- pub/gen/exec.sh
+++ pub/gen/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/vitry/vitry.new"
-/usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/common/com.new"
+/usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/preparation/vitry/vitry.new"
+/usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/preparation/common/com.new"
 /usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/compensation/verniquet.new"
 /usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/reseau/all_compensation/micro.new"
