d58a811ea5812f2807609da2db8f6e5af4a50ae6
Lapin-solitaire <yannmeneroux@hotmail.fr>
2024-01-02 01:36:25 +0100

    Traitement obs Guilands II

--- pub/gen/exec.sh
+++ pub/gen/exec.sh
@@ -3,4 +3,4 @@
 /usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/vitry/vitry.new"
 /usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/common/com.new"
 /usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/compensation/verniquet.new"
-
+/usr/bin/python3 pub.py "/home/YMeneroux/Bureau/KitYann/Recherches/Verniquet/verniquet/micro/reseau/all_compensation/micro.new"
